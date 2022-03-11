40aeb205fcad88a30d87500be365e04b5c193396
sachin sahare <sachin.sahare@outlook.com>
2022-03-11 14:15:16 +0530

    Modified files

---
+++ Sachu.sh
@@ -0,0 +1,15 @@
+echo "Hellow Dear Pooja"
+
+Date
+
+
+echo "Hope you are doing well."
+
+pwd
+
+echo "We all miss you dear bcoz we all are more enjoying without you"
+
+
+echo "I want to be a business man as it is my dream to be the richest person in the world" >> file4
+
+cat file3 >> file4
